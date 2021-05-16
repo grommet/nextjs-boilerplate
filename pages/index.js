bde2f8d44e67b73035e22869565cccc4db2394f0
shimisun <shimrit.yacobi@gmail.com>
2021-05-16 15:19:00 -0600

    Updated packages

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,147 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+
+import {
+  Anchor,
+  Avatar,
+  Box,
+  Button,
+  Footer,
+  Grommet,
+  Heading,
+  Nav,
+  Paragraph,
+} from "grommet";
+
+import {
+  CircleInformation,
+  Grommet as GrommetIcon,
+  Github,
+  Slack,
+} from "grommet-icons";
+/**
+ *
+ * @returns Removed css/className
+ * Replaced html tags with grommet components
+ */
 
 export default function Home() {
+  const gravatarLink =
+    "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80";
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Grommet full>
+      <Box
+        // pad={{ horizontal: "xlarge", top: "large" }}
+        // align="center"
+        fill
+        flex
+        margin={{ horizontal: "auto" }}
+        width={{ max: "xlarge" }}
+        height={{ min: "100%" }}
+        width={{ max: "xlarge" }}
+      >
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Box role="main" pad={{ vertical: "large" }}>
+          <Heading>
+            Welcome to <Anchor href="https://nextjs.org">Next.js</Anchor> and{" "}
+            <Anchor href="https://v2.grommet.io">Grommet!</Anchor>
+          </Heading>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <Paragraph fill>
+            This application is a boilerplate for using Next.js Framework, React
+            library and the Grommet Component Library.
+          </Paragraph>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Paragraph fill>
+            The application and the page you are currently viewing is the
+            default page that is created after bootstrapping a Next.js with{" "}
+            <Anchor href="https://nextjs.org/docs/api-reference/create-next-app">
+              Create Next App
+            </Anchor>
+            .
+          </Paragraph>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+          <Paragraph fill>
+            To the default Create Next App application we added the grommet
+            dependency, and replaced the HTML tags with actual grommet
+            components, as a result you are viewing the same default page, with
+            only Grommet components.
+          </Paragraph>
+
+          <Paragraph fill>
+            Feel free to shoot the Grommet team any feedback and questions by
+            using this page footer contact info.
+          </Paragraph>
+
+          <Paragraph fill>
+            Get started by editing <code>pages/index.js</code>
+          </Paragraph>
+
+          <Box>
+            <Anchor href="https://nextjs.org/docs">
+              <Heading level={3}>Documentation &rarr;</Heading>
+            </Anchor>
+            <Paragraph>
+              Find in-depth information about Next.js features and API.
+            </Paragraph>
+
+            <Anchor href="https://nextjs.org/learn">
+              <Heading level={3}>Learn &rarr;</Heading>
+            </Anchor>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <Anchor href="https://github.com/vercel/next.js/tree/master/examples">
+              <Heading level={3}>Examples &rarr;</Heading>
+            </Anchor>
+            <Paragraph>
+              Discover and deploy boilerplate example Next.js projects.
+            </Paragraph>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
+            <Anchor href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+              <Heading level={3}>Deploy &rarr;</Heading>
+            </Anchor>
+            <Paragraph>
               Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            </Paragraph>
+          </Box>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Anchor
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Next.js Powered by Vercel
+          </Anchor>
+        </Box>
+        <Footer
+          background="light-2"
+          pad={{ vertical: "small", horizontal: "medium" }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+          <Anchor href="https://github.com/ShimiSun">
+            <Avatar src={gravatarLink} />
+          </Anchor>
+          <Nav direction="row" align="center">
+            <Anchor
+              a11yTitle="Reach out to the Grommet Community on Slack"
+              href="https://slack-invite.grommet.io/"
+              icon={<Slack color="plain" />}
+              target="_blank"
+              rel="noreferrer noopener"
+            />
+            <Anchor
+              a11yTitle="Github repository"
+              href="https://github.com/grommet/interns-2021"
+              icon={<Github color="black" />}
+              target="_blank"
+              rel="noreferrer noopener"
+            />
+          </Nav>
+        </Footer>
+      </Box>
+    </Grommet>
+  );
 }
